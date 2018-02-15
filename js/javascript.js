8945f0c1950825c521d397b73d6a5ea5ad3deec6
Alexander <amwow@live.dk>
2018-02-15 13:06:35 +0100

    Malfunktionel garn-kode, og tilføjet garnkugle - AM

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,7 @@ scroller.e = document.getElementById("b");
 
 if (scroller.e.addEventListener) 
 {
+    
     scroller.e.addEventListener("mousewheel", MouseWheelHandler, false);
     scroller.e.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
 } else scroller.e.attachEvent("onmousewheel", MouseWheelHandler);
@@ -15,16 +16,42 @@ function MouseWheelHandler(e)
     var delta = - 20 * (Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))));
 
     var pst = $('#b').scrollLeft() + delta;
-
+    
+    var garn = document.getElementById("yarn");
+   
+    var tal = 0;
+    
+    if(scroller.e.scrollLeft+20)
+        {
+            console.log("X ER = " + scroller.e.scrollLeft);
+            
+            tal++;
+            console.log(tal);
+            
+            garn.style.transform = "rotate(20deg)";
+        }
+    
     if (pst < 0) 
     {
         pst = 0;
+        
     } else if (pst > $('img_holder').width()) 
     {
-        pst = $('img_holder').width();
+        pst = $('img_holder').width(); 
+        
     }
 
     $('#b').scrollLeft(pst);
 
+
+    
+    
     return false;
-}    
+    
+    
+}
+
+ 
+
+
+
